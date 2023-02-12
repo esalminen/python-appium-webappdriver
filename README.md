6c0fd54ae30b84b46402b4f47e9f2c224dee6d89
Esa Salminen <esa_salminen@hotmail.com>
2023-02-12 11:48:41 +0200

    updated: readme

--- README.md
+++ README.md
@@ -7,4 +7,14 @@
 - You also need some windows element inspecting tool. **[Here](https://learn.microsoft.com/en-us/windows/win32/winauto/inspect-objects)** is info about Accessibility Insights which Microsoft recommends and **[here](https://learn.microsoft.com/en-us/windows/win32/winauto/inspect-objects)** is more info about Microsoft Inspect.exe which is a legacy tool.
 
 ## C# Project RPA_TestApp
-Is a simple C# test application for file `desktopdemo.py` where each of the UI buttons print different text to the middle of the form. Ui tests that the text is correct after pressing each of the four buttons.
+Is a simple C# test application for file `desktopdemo.py` where each of the UI buttons print different text to the middle of the form. Appium tests that the text is correct after pressing each of the four buttons.
+
+## How to use the Appium with Windows Forms application
+ #### The following table shows the mapping between the locators in Appium and Inspector.
+
+    Appium WebDriver locator            Accessibility Insight / Inspect.exe property
+    ------------------------------------------------------------------------------------
+    find_element_by_name                Name
+    find_element_by_accessibility_id    AutomationId
+    find_element_by_classname           ClassName   
+    find_element_by_id                  RuntimeId
--- ordertest.py
+++ ordertest.py
@@ -2,7 +2,6 @@ import os
 import unittest
 from appium import webdriver # appium 1.3.0, had trouble with latest version
 
-
 class SimpleCalculatorTests(unittest.TestCase):
 
     @classmethod
@@ -24,31 +23,35 @@ class SimpleCalculatorTests(unittest.TestCase):
     def tearDownClass(self):
         self.driver.quit()
 
-    # The locators supported by WinAppDriver are similar to the ones in Selenium WebDriver.
-    # The following table shows the mapping between the locators in Selenium WebDriver and WinAppDriver.
+    # The following table shows the mapping between the locators in Appium and Inspector.
     #
-    # Selenium WebDriver locator    Locator strategy    Inspect.exe property
-    # --------------------------------------------------------------------------------------------
-    # FindElementByAccessibilityId  accessibility id    AutomationId
-    # FindElementByClassName        class name          ClassName   
-    # FindElementById               id                  RuntimeId(decimal)
-    # FindElementByName             name                Name
-    # FindElementByTagName          tag name            LocalizedControlType(upper camel case)
+    # Appium WebDriver locator      Inspect.exe property
+    # ------------------------------------------------------------------------------------
+    # FindElementByAccessibilityId  AutomationId
+    # FindElementByClassName        ClassName   
+    # FindElementById               RuntimeId(decimal)
+    # FindElementByName             Name
+    # FindElementByTagName          LocalizedControlType(upper camel case)
 
     def selectproduct(self, product):
-        self.driver.find_element_by_accessibility_id("cbSortCriterion").click()
-        self.driver.find_element_by_accessibility_id("cbSortCriterion").find_element_by_id(product).click()
-        displayResult = self.driver.find_element_by_accessibility_id("cbSortCriterion")
-        return displayResult
+        productComboBox = self.driver.find_element_by_accessibility_id("cbSortcriterion")
+        productComboBox.click()
+        productComboBox.send_keys(f'{product}\n')
 
     def insertAmount(self, amount):
-        # insert value to amount text box
         self.driver.find_element_by_accessibility_id("tbProductPcs").send_keys(amount)
 
+    def selectDoor(self, door):
+        # select door
+        doorComboBox = self.driver.find_element_by_accessibility_id("cbDestination")
+        doorComboBox.click()
+        # doorComboBox.send_keys(f'{door}\n')
+        doorComboBox.send_keys()
+
     def test_selectProduct(self):
-        #self.selectproduct(0)
+        self.selectproduct('1122')
         self.insertAmount(5)
-        self.assertEqual(self.getresults(), "Hello")
+        self.selectDoor('Door 1')
 
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(SimpleCalculatorTests)
